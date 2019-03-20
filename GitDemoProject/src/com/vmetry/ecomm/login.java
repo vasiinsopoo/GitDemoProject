182e89c71de78cc5696cd3803067e51354b97fe7
vasiinsopoo <40399772+vasiinsopoo@users.noreply.github.com>
2019-03-20 21:27:31 +0530

    New proj
    Added

---
+++ GitDemoProject/src/com/vmetry/ecomm/login.java
@@ -0,0 +1,12 @@
+package com.vmetry.ecomm;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" this is test proj");
+		
+	}
+
+}
